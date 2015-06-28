bd132142ae71bd65a2e48fc1410cde837af0c202
Ryan Drake <ryan@hdnastudio.com>
2015-06-27 23:52:21 -0700

    Make CFStreamCreatePairWithUnixSocket more robust to input strings

--- TournamentKit/CFStreamCreatePairWithUnixSocket.c
+++ TournamentKit/CFStreamCreatePairWithUnixSocket.c
@@ -3,6 +3,7 @@
 #include <CFNetwork/CFSocketStream.h>
 #include <CoreFoundation/CFNumber.h>
 
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
@@ -12,12 +13,29 @@
 void CFStreamCreatePairWithUnixSocket(CFAllocatorRef alloc, CFStringRef name, CFReadStreamRef* readStream, CFWriteStreamRef* writeStream)
 {
     struct sockaddr_un addr;
+    char buffer[FILENAME_MAX];
+    const char* path;
+
+    // Clear streams
+    if(readStream) {
+        *readStream = NULL;
+    }
+    if(writeStream) {
+        *writeStream = NULL;
+    }
 
     // Check path length
-    const char* path = CFStringGetCStringPtr(name, kCFStringEncodingUTF8);
+    path = CFStringGetCStringPtr(name, kCFStringEncodingUTF8);
+    if(path == NULL) {
+        if(!CFStringGetCString(name, buffer, FILENAME_MAX, kCFStringEncodingUTF8)) {
+            fprintf(stderr, "Filename buffer not large enough\n");
+            return;
+        }
+        path = buffer;
+    }
     if(strlen(path) > sizeof(addr.sun_path)-1)
     {
-        fprintf(stderr, "Socket name too long");
+        fprintf(stderr, "Socket name too long: %s\n", path);
         return;
     }
 
@@ -30,7 +48,7 @@ void CFStreamCreatePairWithUnixSocket(CFAllocatorRef alloc, CFStringRef name, CF
     int sock = socket(PF_UNIX, SOCK_STREAM, 0);
     if(sock == -1)
     {
-        fprintf(stderr, "Call to socket failed");
+        fprintf(stderr, "Call to socket failed: %s\n", strerror(errno));
         return;
     }
 
@@ -38,14 +56,14 @@ void CFStreamCreatePairWithUnixSocket(CFAllocatorRef alloc, CFStringRef name, CF
     int yes = 1;
     if(setsockopt(sock, SOL_SOCKET, SO_NOSIGPIPE, &yes, sizeof(yes)) == -1)
     {
-        fprintf(stderr, "Call to setsockopt failed");
+        fprintf(stderr, "Call to setsockopt failed: %s\n", strerror(errno));
         close(sock);
         return;
     }
 
     if(connect(sock, (struct sockaddr*)&addr, addr.sun_len) == -1)
     {
-        fprintf(stderr, "Call to connect failed");
+        fprintf(stderr, "Call to connect failed: %s\n", strerror(errno));
         close(sock);
         return;
     }
