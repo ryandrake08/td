dcb94def98d4213470ff5ebdfbd58b2f6d6a3be2
Ryan Drake <ryan@hdnastudio.com>
2015-08-26 08:46:13 -0700

    Remove clever, but for now non-portable constexpr crc32

--- tournamentd/program.cpp
+++ tournamentd/program.cpp
@@ -1,6 +1,11 @@
 #include "program.hpp"
-#include "stringcrc.hpp"
 #include <sstream>
+
+static const char* usage =
+    "Usage: tournamentd [options]\n"
+    " -c, --conf FILE\tInitialize configuration from file\n"
+    " -a, --auth LIST\tPre-authorize client authentication code\n"
+    " -n, --name NAME\tPublish Bonjour service with given name (default: tournamentd)\n";
 
 program::program(const std::vector<std::string>& cmdline)
 {
@@ -17,44 +22,55 @@ program::program(const std::vector<std::string>& cmdline)
     // parse command-line
     for(auto it(cmdline.begin()+1); it != cmdline.end();)
     {
-        switch(crc32(*it++))
+        auto cmd(*it++);
+
+        if(cmd == "-c" || cmd == "--conf")
         {
-            case crc32_("-c"):
-            case crc32_("--conf"):
-                if(it != cmdline.end())
-                {
-                    // load supplied config
-                    this->tourney.load_configuration(*it++);
-                }
-                break;
-
-            case crc32_("-a"):
-            case crc32_("--auth"):
-                if(it != cmdline.end())
-                {
-                    // parse client code
-                    this->tourney.authorize(std::stoi(*it++));
-                }
-                break;
-
-            case crc32_("-n"):
-            case crc32_("--name"):
-                if(it != cmdline.end())
-                {
-                    name = *it++;
-                }
-                break;
-
-            case crc32_("-h"):
-            case crc32_("--help"):
-            default:
-                std::cerr << "Unknown option: " << *it << "\n"
-                             "Usage: tournamentd [options]\n"
-                             " -c, --conf FILE\tInitialize configuration from file\n"
-                             " -a, --auth LIST\tPre-authorize client authentication code\n"
-                             " -n, --name NAME\tPublish Bonjour service with given name (default: tournamentd)\n";
+            if(it != cmdline.end())
+            {
+                // load supplied config
+                this->tourney.load_configuration(*it++);
+            }
+            else
+            {
+                std::cerr << "No parameter for " << cmd << "\n" << usage;
                 std::exit(EXIT_FAILURE);
-                break;
+            }
+        }
+        else if(cmd == "-a" || cmd == "--auth")
+        {
+            if(it != cmdline.end())
+            {
+                // parse client code
+                this->tourney.authorize(std::stoi(*it++));
+            }
+            else
+            {
+                std::cerr << "No parameter for " << cmd << "\n" << usage;
+                std::exit(EXIT_FAILURE);
+            }
+        }
+        else if(cmd == "-n" || cmd == "--name")
+        {
+            if(it != cmdline.end())
+            {
+                name = *it++;
+            }
+            else
+            {
+                std::cerr << "No parameter for " << cmd << "\n" << usage;
+                std::exit(EXIT_FAILURE);
+            }
+        }
+        else if(cmd == "-h" || cmd == "--help")
+        {
+            std::cerr << usage;
+            std::exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            std::cerr << "Unknown option: " << *it << "\n" << usage;
+            std::exit(EXIT_FAILURE);
         }
     }
 
