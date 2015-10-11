cff3ce85affa34bec7b600ce62e5dc9978a4c5a4
Ryan Drake <ryan@hdnastudio.com>
2015-10-10 17:10:38 -0700

    Disable scope_timer when not compiling with clang

--- tournamentd/scope_timer.cpp
+++ tournamentd/scope_timer.cpp
@@ -8,7 +8,10 @@
 
 #include "scope_timer.hpp"
 #include "logger.hpp"
+
+#if defined(__clang__)
 #include "chrono/chrono_io"
+#endif
 
 scope_timer::scope_timer() : begin_time(std::chrono::high_resolution_clock::now())
 {
@@ -24,7 +27,11 @@ scope_timer::~scope_timer()
     end_time = std::chrono::high_resolution_clock::now();
 
     // log
+#if defined(__clang__)
     logger(LOG_DEBUG) << this->log_message << (this->end_time - this->begin_time) << '\n';
+#else
+    logger(LOG_DEBUG) << this->log_message << "scope_timer only available when built using clang\n";
+#endif
 }
 
 void scope_timer::set_message(const std::string& message)
