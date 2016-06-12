7a7799870ba31ae328557b8ed106ab7b4669b3b5
Ryan Drake <ryan@hdnastudio.com>
2016-06-12 13:02:45 -0400

    Refactor/modernize ObservableDictionary

--- TBWin/ObservableDictionary.cs
+++ TBWin/ObservableDictionary.cs
@@ -3,12 +3,13 @@
 //and to conform to the PCL declaration of NotifyCollectionChangedEventArgs
 
 using System;
-using System.Linq;
-using System.ComponentModel;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
 
-namespace System.Collections.ObjectModel
+namespace TBWin
 {
     public class ObservableDictionary<TKey, TValue> : IDictionary<TKey, TValue>, INotifyCollectionChanged, INotifyPropertyChanged
     {
@@ -17,36 +18,32 @@ namespace System.Collections.ObjectModel
         private const string KeysName = "Keys";
         private const string ValuesName = "Values";
 
-        private IDictionary<TKey, TValue> _Dictionary;
-        protected IDictionary<TKey, TValue> Dictionary
-        {
-            get { return _Dictionary; }
-        }
+        protected IDictionary<TKey, TValue> Dictionary { get; private set; }
 
         #region Constructors
         public ObservableDictionary()
         {
-            _Dictionary = new Dictionary<TKey, TValue>();
+            Dictionary = new Dictionary<TKey, TValue>();
         }
         public ObservableDictionary(IDictionary<TKey, TValue> dictionary)
         {
-            _Dictionary = new Dictionary<TKey, TValue>(dictionary);
+            Dictionary = new Dictionary<TKey, TValue>(dictionary);
         }
         public ObservableDictionary(IEqualityComparer<TKey> comparer)
         {
-            _Dictionary = new Dictionary<TKey, TValue>(comparer);
+            Dictionary = new Dictionary<TKey, TValue>(comparer);
         }
         public ObservableDictionary(int capacity)
         {
-            _Dictionary = new Dictionary<TKey, TValue>(capacity);
+            Dictionary = new Dictionary<TKey, TValue>(capacity);
         }
         public ObservableDictionary(IDictionary<TKey, TValue> dictionary, IEqualityComparer<TKey> comparer)
         {
-            _Dictionary = new Dictionary<TKey, TValue>(dictionary, comparer);
+            Dictionary = new Dictionary<TKey, TValue>(dictionary, comparer);
         }
         public ObservableDictionary(int capacity, IEqualityComparer<TKey> comparer)
         {
-            _Dictionary = new Dictionary<TKey, TValue>(capacity, comparer);
+            Dictionary = new Dictionary<TKey, TValue>(capacity, comparer);
         }
         #endregion
 
@@ -62,10 +59,8 @@ namespace System.Collections.ObjectModel
             return Dictionary.ContainsKey(key);
         }
 
-        public ICollection<TKey> Keys
-        {
-            get { return Dictionary.Keys; }
-        }
+        public ICollection<TKey> Keys => Dictionary.Keys;
+        public ICollection<TValue> Values => Dictionary.Values;
 
         public bool Remove(TKey key)
         {
@@ -83,11 +78,6 @@ namespace System.Collections.ObjectModel
         public bool TryGetValue(TKey key, out TValue value)
         {
             return Dictionary.TryGetValue(key, out value);
-        }
-
-        public ICollection<TValue> Values
-        {
-            get { return Dictionary.Values; }
         }
 
         public TValue this[TKey key]
@@ -131,15 +121,8 @@ namespace System.Collections.ObjectModel
             Dictionary.CopyTo(array, arrayIndex);
         }
 
-        public int Count
-        {
-            get { return Dictionary.Count; }
-        }
-
-        public bool IsReadOnly
-        {
-            get { return Dictionary.IsReadOnly; }
-        }
+        public int Count => Dictionary.Count;
+        public bool IsReadOnly => Dictionary.IsReadOnly;
 
         public bool Remove(KeyValuePair<TKey, TValue> item)
         {
@@ -187,13 +170,13 @@ namespace System.Collections.ObjectModel
             {
                 if (Dictionary.Count > 0)
                 {
-                    if (items.Keys.Any((k) => Dictionary.ContainsKey(k)))
+                    if (items.Keys.Any(k => Dictionary.ContainsKey(k)))
                         throw new ArgumentException("An item with the same key has already been added.");
                     else
                         foreach (var item in items) Dictionary.Add(item);
                 }
                 else
-                    _Dictionary = new Dictionary<TKey, TValue>(items);
+                    Dictionary = new Dictionary<TKey, TValue>(items);
 
                 OnCollectionChanged(NotifyCollectionChangedAction.Add, items.ToArray());
             }
@@ -232,31 +215,31 @@ namespace System.Collections.ObjectModel
 
         protected virtual void OnPropertyChanged(string propertyName)
         {
-            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private void OnCollectionChanged()
         {
             OnPropertyChanged();
-            if (CollectionChanged != null) CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
         }
 
         private void OnCollectionChanged(NotifyCollectionChangedAction action, KeyValuePair<TKey, TValue> changedItem)
         {
             OnPropertyChanged();
-            if (CollectionChanged != null) CollectionChanged(this, new NotifyCollectionChangedEventArgs(action, changedItem, 0));
+            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(action, changedItem, 0));
         }
 
         private void OnCollectionChanged(NotifyCollectionChangedAction action, KeyValuePair<TKey, TValue> newItem, KeyValuePair<TKey, TValue> oldItem)
         {
             OnPropertyChanged();
-            if (CollectionChanged != null) CollectionChanged(this, new NotifyCollectionChangedEventArgs(action, newItem, oldItem, 0));
+            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(action, newItem, oldItem, 0));
         }
 
         private void OnCollectionChanged(NotifyCollectionChangedAction action, IList newItems)
         {
             OnPropertyChanged();
-            if (CollectionChanged != null) CollectionChanged(this, new NotifyCollectionChangedEventArgs(action, newItems, 0));
+            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(action, newItems, 0));
         }
     }
 }
