f0be4e9921f200e4b42f8779d50b7b060d51e2a6
Ryan Drake <ryan@hdnastudio.com>
2017-12-18 08:21:06 -0800

    Move program object over to using pimpl

--- tournamentd/program.cpp
+++ tournamentd/program.cpp
@@ -1,97 +1,122 @@
 #include "program.hpp"
+#include "bonjour.hpp"
 #include "logger.hpp"
-#include <sstream>
+#include "tournament.hpp"
 
 #if !defined(P_tmpdir)
 #define	P_tmpdir "/tmp/"
 #endif
 
-static const char* usage =
-    "Usage: tournamentd [options]\n"
-    " -c, --conf FILE\tInitialize configuration from file\n"
-    " -a, --auth LIST\tPre-authorize client authentication code\n"
-    " -n, --name NAME\tPublish Bonjour service with given name (default: tournamentd)\n";
-
-program::program(const std::vector<std::string>& cmdline)
+struct program::impl
 {
-    std::string name("tournamentd");
+    bonjour_publisher publisher;
+    tournament tourney;
+
+public:
+    explicit impl(const std::vector<std::string>& cmdline)
+    {
+        std::string name("tournamentd");
+
+        static const char* usage =
+            "Usage: tournamentd [options]\n"
+            " -c, --conf FILE\tInitialize configuration from file\n"
+            " -a, --auth LIST\tPre-authorize client authentication code\n"
+            " -n, --name NAME\tPublish Bonjour service with given name (default: tournamentd)\n";
 
 #if defined(DEBUG)
-    // debug only: always accept this code
-    this->tourney.authorize(31337);
+        // debug only: always accept this code
+        this->tourney.authorize(31337);
 
-    // debug only: load a default configuration
-    this->tourney.load_configuration("defaults.json");
+        // debug only: load a default configuration
+        this->tourney.load_configuration("defaults.json");
 #else
-    // disable LOG_DEBUG
-    logger_enable(LOG_INFO, LOG_WARNING, LOG_ERROR);
+        // disable LOG_DEBUG
+        logger_enable(LOG_INFO, LOG_WARNING, LOG_ERROR);
 #endif
 
-    // parse command-line
-    for(auto it(cmdline.begin()+1); it != cmdline.end();)
-    {
-        auto cmd(*it++);
+        // parse command-line
+        for(auto it(cmdline.begin()+1); it != cmdline.end();)
+        {
+            auto cmd(*it++);
 
-        if(cmd == "-c" || cmd == "--conf")
-        {
-            if(it != cmdline.end())
+            if(cmd == "-c" || cmd == "--conf")
             {
-                // load supplied config
-                this->tourney.load_configuration(*it++);
+                if(it != cmdline.end())
+                {
+                    // load supplied config
+                    this->tourney.load_configuration(*it++);
+                }
+                else
+                {
+                    std::cerr << "No parameter for " << cmd << "\n" << usage;
+                    std::exit(EXIT_FAILURE);
+                }
+            }
+            else if(cmd == "-a" || cmd == "--auth")
+            {
+                if(it != cmdline.end())
+                {
+                    // parse client code
+                    this->tourney.authorize(std::stoi(*it++));
+                }
+                else
+                {
+                    std::cerr << "No parameter for " << cmd << "\n" << usage;
+                    std::exit(EXIT_FAILURE);
+                }
+            }
+            else if(cmd == "-n" || cmd == "--name")
+            {
+                if(it != cmdline.end())
+                {
+                    name = *it++;
+                }
+                else
+                {
+                    std::cerr << "No parameter for " << cmd << "\n" << usage;
+                    std::exit(EXIT_FAILURE);
+                }
+            }
+            else if(cmd == "-h" || cmd == "--help")
+            {
+                std::cerr << usage;
+                std::exit(EXIT_SUCCESS);
             }
             else
             {
-                std::cerr << "No parameter for " << cmd << "\n" << usage;
+                std::cerr << "Unknown option: " << *it << "\n" << usage;
                 std::exit(EXIT_FAILURE);
             }
         }
-        else if(cmd == "-a" || cmd == "--auth")
-        {
-            if(it != cmdline.end())
-            {
-                // parse client code
-                this->tourney.authorize(std::stoi(*it++));
-            }
-            else
-            {
-                std::cerr << "No parameter for " << cmd << "\n" << usage;
-                std::exit(EXIT_FAILURE);
-            }
-        }
-        else if(cmd == "-n" || cmd == "--name")
-        {
-            if(it != cmdline.end())
-            {
-                name = *it++;
-            }
-            else
-            {
-                std::cerr << "No parameter for " << cmd << "\n" << usage;
-                std::exit(EXIT_FAILURE);
-            }
-        }
-        else if(cmd == "-h" || cmd == "--help")
-        {
-            std::cerr << usage;
-            std::exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            std::cerr << "Unknown option: " << *it << "\n" << usage;
-            std::exit(EXIT_FAILURE);
-        }
+
+        // listen and publish
+        this->publisher.publish(name.c_str(), this->tourney.listen(P_tmpdir).second);
     }
 
-    // listen and publish
-    this->publisher.publish(name.c_str(), this->tourney.listen(P_tmpdir).second);
+    bool run()
+    {
+        return this->tourney.run();
+    }
+
+    bool sigusr2()
+    {
+        logger(LOG_INFO) << "user signal\n";
+        return false;
+    }
+};
+
+program::program(const std::vector<std::string>& cmdline) : pimpl(new impl(cmdline))
+{
 }
+
+program::~program() = default;
 
 bool program::run()
 {
-    return this->tourney.run();
+    return this->pimpl->run();
 }
 
 bool program::sigusr2()
 {
-    return false;
+    return this->pimpl->sigusr2();
 }
--- tournamentd/program.hpp
+++ tournamentd/program.hpp
@@ -1,21 +1,24 @@
 #pragma once
+#include <memory>
 #include <string>
 #include <vector>
-#include "tournament.hpp"
-#include "bonjour.hpp"
 
 class program
 {
-    bonjour_publisher publisher;
-    tournament tourney;
+    // pimpl
+    struct impl;
+    std::unique_ptr<impl> pimpl;
 
 public:
     // Create a program, given command line options
     explicit program(const std::vector<std::string>& cmdline);
 
+    // Destructor
+    ~program();
+
     // Run one iteration of the program run loop, returns true to exit
     bool run();
 
-    // Handle SIGUSR2 user event, returning true, returns true to exit
+    // Handle SIGUSR2 user event, returns true to exit
     bool sigusr2();
 };
