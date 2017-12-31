05f5ad536f07bde503ef5cc675661e93a774f479
Ryan Drake <ryan@hdnastudio.com>
2017-12-31 01:28:07 -0800

    Flatten all of the play screen's container views into a single view controller

--- TBMac/TBPlayerViewController.m
+++ TBMac/TBPlayerViewController.m
@@ -8,37 +8,57 @@
 
 #import "TBPlayerViewController.h"
 #import "TBActionClockViewController.h"
-#import "TBResizeTextField.h"
+#import "TBCurrencyNumberFormatter.h"
 #import "TBSoundPlayer.h"
 #import "NSObject+FBKVOController.h"
 
-@interface TBPlayerViewController ()
+@interface TBPlayerViewController () <NSTableViewDelegate>
 
 // UI elements
 @property (weak) IBOutlet NSImageView* backgroundImageView;
+@property (weak) IBOutlet NSButton* previousRoundButton;
+@property (weak) IBOutlet NSButton* pauseResumeButton;
+@property (weak) IBOutlet NSButton* nextRoundButton;
+@property (weak) IBOutlet NSButton* callClockButton;
+@property (weak) IBOutlet NSTableView* chipsTableView;
+@property (weak) IBOutlet NSTableView* resultsTableView;
 
 // Sound player
 @property (strong) IBOutlet TBSoundPlayer* soundPlayer;
 
-// Containers
-@property (strong) NSMutableArray* containerViewControllers;
+// Array controller for objects managed by this view controller
+@property (strong) IBOutlet NSArrayController* resultsArrayController;
 
 @end
 
 @implementation TBPlayerViewController
-
-@synthesize session = _session;
 
 - (void)viewDidLoad {
     if([NSViewController instancesRespondToSelector:@selector(viewDidLoad)]) {
         [super viewDidLoad];
     }
 
-    _containerViewControllers = [[NSMutableArray alloc] init];
+    // update buttons when authorization, connected, or current_blinc_level changes
+    [[self KVOController] observe:self keyPaths:@[@"session.state.connected", @"session.state.authorized", @"session.state.current_blind_level"] options:NSKeyValueObservingOptionInitial block:^(id observer, TBPlayerViewController* object, NSDictionary *change) {
+        BOOL authorized = [[[object session] state][@"connected"] boolValue] && [[[object session] state][@"authorized"] boolValue];
+        BOOL playing = [[[object session] state][@"current_blind_level"] unsignedIntegerValue] != 0;
+        [[observer previousRoundButton] setEnabled:authorized && playing];
+        [[observer pauseResumeButton] setEnabled:authorized];
+        [[observer nextRoundButton] setEnabled:authorized && playing];
+        [[observer callClockButton] setEnabled:authorized && playing];
+    }];
 
+    // update action clock when action_clock_time_remaining chantes
     [[self KVOController] observe:self keyPath:@"session.state.action_clock_time_remaining" options:0 block:^(id observer, id object, NSDictionary *change) {
         [observer updateActionClock:[[object session] state][@"action_clock_time_remaining"]];
     }];
+
+    // chips tableview reloads itself when available_chips changes
+    [[[self chipsTableView] KVOController] observe:self keyPath:@"session.state.available_chips" options:0 action:@selector(reloadData)];
+
+    // set up sort descriptor for results
+    NSSortDescriptor* placeSort = [[NSSortDescriptor alloc] initWithKey:@"place" ascending:YES];
+    [[self resultsArrayController] setSortDescriptors:@[placeSort]];
 
     // set up sound player
     [[self soundPlayer] setSession:[self session]];
@@ -52,26 +72,28 @@
 }
 
 - (void)prepareForSegue:(NSStoryboardSegue *)segue sender:(id)sender {
-    id dc = [segue destinationController];
-    if([dc respondsToSelector:@selector(setSession:)]) {
-        [[self containerViewControllers] addObject:dc];
-    }
-
     if([[segue identifier] isEqualToString:@"presentActionClockView"]) {
         TBActionClockViewController* vc =  [segue destinationController];
         [vc setSession:[self session]];
     }
 }
 
-- (void)setSession:(TournamentSession*)session {
-    _session = session;
-    for(id e in [self containerViewControllers]) {
-        [e setSession:session];
-    }
-}
+#pragma mark NSTableViewDelegate
 
-- (TournamentSession*)session {
-    return _session;
+- (NSView *)tableView:(NSTableView*)aTableView viewForTableColumn:(NSTableColumn*)aTableColumn row:(NSInteger)rowIndex {
+    NSTableCellView* result = [aTableView makeViewWithIdentifier:aTableColumn.identifier owner:self];
+    if([[aTableColumn identifier] isEqualToString:@"Payout"]) {
+        if([[result textField] formatter] == nil) {
+            [[result textField] setFormatter:[[TBCurrencyNumberFormatter alloc] init]];
+        }
+        NSDictionary* object = [[[self resultsArrayController] arrangedObjects] objectAtIndex:rowIndex];
+        [[[result textField] formatter] setCurrencyCode:object[@"payout_currency"]];
+    } else if([[aTableColumn identifier] isEqualToString:@"Chip"]) {
+        [[result imageView] bind:@"color" toObject:result withKeyPath:@"objectValue.color" options:@{NSValueTransformerNameBindingOption:@"TBColorValueTransformer"}];
+    } else {
+        NSLog(@"viewForTableColumn: %@", [aTableColumn identifier]);
+    }
+    return result;
 }
 
 #pragma mark Update action clock
@@ -83,6 +105,43 @@
         [self dismissViewController:presented[0]];
     } else if(actionClockTimeRemaining > 0 && [presented count] == 0) {
         [self performSegueWithIdentifier:@"presentActionClockView" sender:self];
+    }
+}
+
+#pragma mark Actions
+
+- (IBAction)previousRoundTapped:(NSButton*)sender {
+    NSUInteger currentBlindLevel = [[[self session] state][@"current_blind_level"] unsignedIntegerValue];
+    if(currentBlindLevel != 0) {
+        [[self session] setPreviousLevelWithBlock:nil];
+    }
+}
+
+- (IBAction)pauseResumeTapped:(NSButton*)sender {
+    NSUInteger currentBlindLevel = [[[self session] state][@"current_blind_level"] unsignedIntegerValue];
+    if(currentBlindLevel != 0) {
+        [[self session] togglePauseGame];
+    } else {
+        [[self session] startGame];
+    }
+}
+
+- (IBAction)nextRoundTapped:(NSButton*)sender {
+    NSUInteger currentBlindLevel = [[[self session] state][@"current_blind_level"] unsignedIntegerValue];
+    if(currentBlindLevel != 0) {
+        [[self session] setNextLevelWithBlock:nil];
+    }
+}
+
+- (IBAction)callClockTapped:(NSButton*)sender {
+    NSUInteger currentBlindLevel = [[[self session] state][@"current_blind_level"] unsignedIntegerValue];
+    if(currentBlindLevel != 0) {
+        NSUInteger remaining = [[[self session] state][@"action_clock_time_remaining"] unsignedIntegerValue];
+        if(remaining == 0) {
+            [[self session] setActionClock:@kActionClockRequestTime];
+        } else {
+            [[self session] clearActionClock];
+        }
     }
 }
 
