77ea729220e259ee57a79eefe65dd5947627a468
Ryan Drake <ryan@hdnastudio.com>
2018-03-16 08:19:45 -0700

    localizedStringForCurrencyCode: is iOS10+, macOS10.12+. Add replacement

--- Common/TBCurrencyNumberFormatter.m
+++ Common/TBCurrencyNumberFormatter.m
@@ -59,8 +59,20 @@
                 name = NSLocalizedString(@"Bucks", @"Special custom currency type");
             } else if([code isEqualToString:@"XPT"]) {
                 name = NSLocalizedString(@"Points", @"Special custom currency type");
-            } else {
+            } else if([[NSLocale currentLocale] respondsToSelector:@selector(localizedStringForCurrencyCode:)]) {
                 name = [[NSLocale currentLocale] localizedStringForCurrencyCode:code];
+            } else if([code isEqualToString:@"USD"]) {
+                name = NSLocalizedString(@"US Dollar", @"Currency name");
+            } else if([code isEqualToString:@"EUR"]) {
+                name = NSLocalizedString(@"Euro", @"Currency name");
+            } else if([code isEqualToString:@"INR"]) {
+                name = NSLocalizedString(@"Indian Rupee", @"Currency name");
+            } else if([code isEqualToString:@"GBP"]) {
+                name = NSLocalizedString(@"British Pound", @"Currency name");
+            } else if([code isEqualToString:@"JPY"]) {
+                name = NSLocalizedString(@"Japanese Yen", @"Currency name");
+            } else if([code isEqualToString:@"CNY"]) {
+                name = NSLocalizedString(@"Chinese Yuan", @"Currency name");
             }
             if(name == nil) {
                 name = NSLocalizedString(@"Unknown", @"Unknown currency type");
