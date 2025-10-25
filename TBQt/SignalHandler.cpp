036668cfb87b2e5b0f04b0ad5de4995e6e672c2d
Ryan Drake <ryan@existens.org>
2025-10-24 18:06:29 -0700

    Cleanup / refactor the SignalHandler class

--- TBQt/SignalHandler.cpp
+++ TBQt/SignalHandler.cpp
@@ -1,105 +1,146 @@
 #include "SignalHandler.hpp"
 
+#if !defined(_WIN32)
+
 #include <QDebug>
 #include <QSocketNotifier>
 
 #include <array>
 #include <csignal>
+#include <stdexcept>
+#include <unordered_set>
 
-#if !defined(_WIN32)
 #include <sys/socket.h>
 #include <unistd.h>
 
+// Internal class to manage socket pair for signal handling
+struct socket_pair
+{
+    std::array<int, 2> fds;
+
+    socket_pair()
+    {
+        if(::socketpair(AF_UNIX, SOCK_STREAM, 0, fds.data()) != 0)
+        {
+            throw std::runtime_error("Failed to create socket pair for signal handling");
+        }
+    }
+
+    ~socket_pair()
+    {
+        if(fds[0] != -1)
+        {
+            close(fds[0]);
+        }
+
+        if(fds[1] != -1)
+        {
+            close(fds[1]);
+        }
+    }
+
+    void notify()
+    {
+        char sig = 1;
+        ssize_t result = write(fds[0], &sig, 1);
+        (void)result; // Suppress unused variable warning
+    }
+
+    bool read_notification()
+    {
+        char signal = '\0';
+        ssize_t result = read(fds[1], &signal, 1);
+        return result > 0;
+    }
+
+    int get_read_fd() const
+    {
+        return fds[1];
+    }
+};
+
 namespace
 {
+    // Static registry of all active socket_pair instances
+    std::unordered_set<socket_pair*> socketInstances;
+}
 
-    // File-local static variable for Unix signal handling
-    std::array<int, 2> sigtermFd = { -1, -1 };
-
-} // anonymous namespace
-#endif
-
-SignalHandler::SignalHandler(QObject* parent) : QObject(parent)
+SignalHandler::SignalHandler(QObject* parent)
+try :
+    QObject(parent),
+    sockets(new socket_pair()),
+    notifier(new QSocketNotifier(this->sockets->get_read_fd(), QSocketNotifier::Read, this))
 {
-#if !defined(_WIN32)
-    // Create a socket pair for self-pipe trick
-    if(::socketpair(AF_UNIX, SOCK_STREAM, 0, sigtermFd.data()) == -1)
+    // Connect the SocketNotifier to our signal handler
+    QObject::connect(this->notifier, &QSocketNotifier::activated, this, &SignalHandler::handleSignal);
+
+    // Register this instance
+    socketInstances.insert(this->sockets.get());
+
+    // Install signal handlers only once (when first instance is created)
+    if(socketInstances.size() == 1)
     {
-        qWarning() << "Failed to create signal socket pair";
-        return;
+        struct sigaction term_action {};
+        sigemptyset(&term_action.sa_mask);
+        term_action.sa_flags = SA_RESTART;
+        term_action.sa_handler = [](int /* signum */)
+        {
+            // Write to all registered instances' socket pairs
+            for(socket_pair* sockets : socketInstances)
+            {
+                sockets->notify();
+            }
+        };
+
+        if(sigaction(SIGINT, &term_action, nullptr) == -1)
+        {
+            qWarning() << "Failed to install SIGINT handler";
+        }
+
+        if(sigaction(SIGTERM, &term_action, nullptr) == -1)
+        {
+            qWarning() << "Failed to install SIGTERM handler";
+        }
+
+        if(sigaction(SIGQUIT, &term_action, nullptr) == -1)
+        {
+            qWarning() << "Failed to install SIGQUIT handler";
+        }
     }
-
-    // Create socket notifier to monitor the read end
-    snTerm = new QSocketNotifier(sigtermFd[1], QSocketNotifier::Read, this);
-    QObject::connect(snTerm, &QSocketNotifier::activated, this, &SignalHandler::handleSignal);
-
-    // Install signal handlers for SIGTERM and SIGINT
-    struct sigaction term_action {};
-    sigemptyset(&term_action.sa_mask);
-    term_action.sa_flags = SA_RESTART;
-    term_action.sa_handler = [](int /* signum */)
-    {
-        // Write to socket to wake up Qt event loop
-        char sig = 1;
-        ssize_t result = write(sigtermFd[0], &sig, 1);
-        (void)result; // Suppress unused variable warning
-    };
-
-    if(sigaction(SIGINT, &term_action, nullptr) == -1)
-    {
-        qWarning() << "Failed to install SIGINT handler";
-        return;
-    }
-
-    if(sigaction(SIGTERM, &term_action, nullptr) == -1)
-    {
-        qWarning() << "Failed to install SIGTERM handler";
-        return;
-    }
-
-    if(sigaction(SIGQUIT, &term_action, nullptr) == -1)
-    {
-        qWarning() << "Failed to install SIGQUIT handler";
-        return;
-    }
-#else
-    // Windows doesn't support Unix signals in the same way
-    // Signal handling could be implemented using Windows-specific mechanisms if needed
-    snTerm = nullptr;
-#endif
+}
+catch(const std::exception& e)
+{
+    qWarning() << "SignalHandler initialization failed:" << e.what();
 }
 
 SignalHandler::~SignalHandler()
 {
-#if !defined(_WIN32)
-    if(snTerm)
-    {
-        snTerm->setEnabled(false);
-    }
-
-    if(sigtermFd[0] != -1)
-    {
-        close(sigtermFd[0]);
-    }
-
-    if(sigtermFd[1] != -1)
-    {
-        close(sigtermFd[1]);
-    }
-#endif
+    socketInstances.erase(this->sockets.get());
+    this->notifier->setEnabled(false);
 }
 
 void SignalHandler::handleSignal()
 {
-#if !defined(_WIN32)
-    snTerm->setEnabled(false);
-
-    char signal = '\0';
-    ssize_t result = read(sigtermFd[1], &signal, 1);
-    (void)result; // Suppress unused variable warning
-
+    this->notifier->setEnabled(false);
+    this->sockets->read_notification();
     Q_EMIT shutdownRequested();
-
-    snTerm->setEnabled(true);
-#endif
+    this->notifier->setEnabled(true);
 }
+
+#else
+
+SignalHandler::SignalHandler(QObject* parent) : QObject(parent)
+{
+    // Windows: Signal handling not implemented
+    // Qt applications on Windows handle shutdown via native event loop
+}
+
+SignalHandler::~SignalHandler()
+{
+}
+
+void SignalHandler::handleSignal()
+{
+}
+
+#endif
--- TBQt/SignalHandler.hpp
+++ TBQt/SignalHandler.hpp
@@ -1,14 +1,22 @@
 #pragma once
 
 #include <QObject>
+#include <memory>
 
 class QSocketNotifier;
+
+#if !defined(_WIN32)
+struct socket_pair;
+#endif
 
 class SignalHandler : public QObject
 {
     Q_OBJECT
 
-    QSocketNotifier* snTerm;
+#if !defined(_WIN32)
+    std::unique_ptr<socket_pair> sockets;
+    QSocketNotifier* notifier;
+#endif
 
 public:
     explicit SignalHandler(QObject* parent = nullptr);
